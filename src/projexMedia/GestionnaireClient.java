38325afaf891d695a3001b16c8d532eb97056ae8
Greninja <Greninja@DESKTOP-VU9PBLI>
2017-10-08 15:58:20 -0400

    UpdateJS

--- src/projexMedia/GestionnaireClient.java
+++ src/projexMedia/GestionnaireClient.java
@@ -176,6 +176,7 @@ public class GestionnaireClient {
          _pane.getChildren().add(btnArchiver);
          _pane.getChildren().add(btnConsArch);
     }
+    
     //Formulaire ajouter client
     
     public void ajouterClient(Stage primaryStage){
